08adb3c8d3e1e4578d956f0bdd5c65d67d1922dc
naufaladrian <naufal.adrian904@gmail.com>
2024-03-16 11:28:25 +0700

    tambah header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './components/Header'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <p>test</p>
+      <Header />
     </>
   )
 }
